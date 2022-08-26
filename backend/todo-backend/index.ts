7960806fea842ebb3256a8113ddcb817a9266be5
Kay Kleinvogel <kaykleinvogel@googlemail.com>
2022-08-26 16:44:05 +0200

    feat(todo-be):adding mongo connection

--- backend/todo-backend/index.ts
+++ backend/todo-backend/index.ts
@@ -1,15 +1,39 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://localhost:27017/TodoBackend")
+  .then(console.log("DB connected"));
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
+const taskSchema = new mongoose.Schema({
+  name: String,
+  tags: [String],
+  created: Number,
+});
+const Task = mongoose.model("Task", taskSchema);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.post("/todo", (req: Request, res: Response) => {
+  const newTask = {
+    name: "testTask",
+    tags: ["personal"],
+    created: Date.now(),
+  };
+  const task = new Task(newTask);
+  task.save().then(() => {
+    res.send(newTask);
+  });
 });
 
 app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
