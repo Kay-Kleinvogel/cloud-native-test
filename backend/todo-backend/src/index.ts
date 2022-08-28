f8d492cb4349b79141e1d43a6ea9435d217c811f
Kay Kleinvogel <kaykleinvogel@googlemail.com>
2022-08-28 11:22:39 +0200

    feat(todo-be): creating docker file

--- backend/todo-backend/src/index.ts
+++ backend/todo-backend/src/index.ts
@@ -1,6 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-import { ITask } from "./interfaces/ITask";
+import { ITask } from "../interfaces/ITask";
 
 dotenv.config();
 
