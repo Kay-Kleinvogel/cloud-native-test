53737c2b1dfe9a349ceeb15cd4da16436151dede
Kay Kleinvogel <kaykleinvogel@googlemail.com>
2022-10-31 10:18:48 +0100

    feat(note-creation):send new note to db

--- frontend/note-fe/src/CreateNote/CreateNote.tsx
+++ frontend/note-fe/src/CreateNote/CreateNote.tsx
@@ -1,13 +1,46 @@
-import { Container, Divider, TextField, Typography } from "@mui/material";
+import {
+  Button,
+  Container,
+  Divider,
+  Fab,
+  TextField,
+  Typography,
+} from "@mui/material";
 import React from "react";
 import reactMarkdown from "react-markdown";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 import remarkGfm from "remark-gfm";
-
+import SaveAsIcon from "@mui/icons-material/SaveAs";
+import { useNavigate } from "react-router-dom";
 const CreateNote = () => {
   // state
   const [title, setTitle] = React.useState("Your Title");
   const [text, setText] = React.useState("Your Content");
+
+  const navigate = useNavigate();
+
+  const saveNote = () => {
+    // set the basic object for the note
+    const noteObject = {
+      name: title,
+      notes: text,
+      tags: ["test1", "test2"],
+      participants: ["test1", "test2"],
+    };
+
+    // send the note to the server
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(noteObject),
+    };
+    fetch("http://localhost:8080/notes", requestOptions)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+
+    // redirect to main page
+    navigate("/");
+  };
 
   return (
     <Container>
@@ -49,6 +82,24 @@ const CreateNote = () => {
           <ReactMarkdown children={text} remarkPlugins={[remarkGfm]} />
         </div>
       </div>
+      <Fab
+        color='primary'
+        aria-label='add'
+        variant='extended'
+        component={Button}
+        style={{
+          margin: 0,
+          top: "auto",
+          right: 20,
+          bottom: 20,
+          left: "auto",
+          position: "fixed",
+        }}
+        onClick={saveNote}
+      >
+        <SaveAsIcon sx={{ mr: 1 }} />
+        Save
+      </Fab>
     </Container>
   );
 };
